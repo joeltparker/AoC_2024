6e3cee602a7ab78e953b56460ea772161c075ea5
Joel <jparker357@gatech.edu>
2024-12-01 15:21:20 -0500

    working part 1

--- day1/solution.js
+++ day1/solution.js
@@ -1,6 +1,27 @@
 import fs from 'fs'
 
-const input = fs.readFileSync('./test.txt', 'utf-8').trim().split('\n');
+const input = fs.readFileSync('./input.txt', 'utf-8').trim().split('\n');
 console.log(input)
 
+let rows = input.map(line => line.replaceAll(/\s+/g, ',').split(','));
+let [column1, column2] = [rows.map(row => parseInt(row[0])), rows.map(row => parseInt(row[1]))];
+
+//console.log('column1: ' + column1)
+//console.log('column2: ' + column2)
+
+column1 = column1.sort((a, b) => a - b);
+column2 = column2.sort((a, b) => a - b);
+
+let diff = column1.map((value, index) => 
+                        {
+                        return Math.abs(value - column2[index]);
+                       })
+let sum = diff.reduce((total, value) => {
+    return total + value
+})
+
+//console.log('column1: ' + column1)
+//console.log('column2: ' + column2)
+//console.log('diff: ' + diff)
+console.log('sum: ' + sum)
 
